84fa9d9fd9ce2722fc57ea7e0099e99d2bcdd140
vamsi123-paidi <vamsipaidi251002@gmail.com>
2026-07-04 16:13:20 +0530

    7/4

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// const heading = document.getElementById('heading')
+// console.log(heading)
+// heading.style.color = "red"
+// const heading = document.getElementsByClassName("heading");
+// console.log(heading)
+// heading[0].style.color="red"
+// const h2 = document.getElementsByTagName("h2")
+// h2[0].style.color = "red"
+
+// const heading = document.querySelectorAll('.heading')
+// heading.forEach((index)=>{
+//     index.style.color = "red"
+// })
+
+const heading1 = document.getElementById('heading1')
+// heading1.innerText = "newly added text in step 4"
+heading1.innerHTML = '<h1>hello world </h1>'
